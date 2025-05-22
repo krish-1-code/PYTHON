90bac2d7406d2d55df1cedc5ddc57b13e66bdcff
Krish <krishkushwaha.2063@gmail.com>
2025-05-22 15:48:19 +0545

    code for circle

---
+++ circle1.py
@@ -0,0 +1,6 @@
+# wap to find area of a circle
+
+import math
+radius= float(input("ENTER THE RADIUS OF THE CIRCLE: "))
+area = math.pi * pow(radius,2) 
+print(f"THE AREA OF CIRCLE IS {round(area,2)}")
